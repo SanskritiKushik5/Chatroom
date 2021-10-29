8c01c39c74d1b235b0768d546b6a5b0d4cd88b48
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-10-29 17:33:19 +0530

    Initial setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 8080 || process.env.PORT;
+app.listen(PORT, () => console.log(`Server: ${PORT}`));
